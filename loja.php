27c024b51056223067df6b5013480ec83a4d2600
Marcos Roberto Albrecht <MarcosAlbrecht@users.noreply.github.com>
2018-06-13 16:28:02 -0300

    Initial commit

---
+++ loja.php
@@ -0,0 +1,168 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Academia Mosntrão</title>
+	<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+	<link href="css/style.css" rel='stylesheet' type='text/css' />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700' rel='stylesheet' type='text/css'>
+	<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+	<script src="js/jquery.min.js"></script>
+	<!-- grid-slider -->
+	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+	<script type="text/javascript" src="js/jquery.contentcarousel.js"></script>
+	</head>
+</head>
+<body>
+ <!-- start header_top -->
+	<div class="header">
+	   <div class="container">
+
+		  <div class="header-arrow">
+		     <a href="#menu" class="class scroll"><span> </span </a>
+		  </div>
+	    </div>
+	  </div>
+	<!-- end header_top -->
+	<!-- start header_bottom -->
+	  <div class="header-bottom">
+		 <div class="container">
+			<div class="header-bottom_left">
+				<i class="phone"> </i><span>1-200-346-2986</span>
+			</div>
+			<div class="social">
+			   <ul>
+				  <li class="facebook"><a href="#"><span> </span></a></li>
+				  <li class="twitter"><a href="#"><span> </span></a></li>
+				  <li class="pinterest"><a href="#"><span> </span></a></li>
+				  <li class="google"><a href="#"><span> </span></a></li>
+				  <li class="tumblr"><a href="#"><span> </span></a></li>
+				  <li class="instagram"><a href="#"><span> </span></a></li>
+				  <li class="rss"><a href="#"><span> </span></a></li>
+			   </ul>
+		   </div>
+		   <div class="clear"></div>
+		</div>
+	  </div>
+	<!-- end header_bottom -->
+	<!-- start menu -->
+    <div class="menu" id="menu">
+		  <div class="container">
+			 <div class="logo">
+				<a href="index.html"><img src="images/logo.png" alt=""/></a>
+			 </div>
+			 <!-- start h_menu4 -->
+			 <div class="h_menu4">
+			   <a class="toggleMenu" href="#">Menu</a>
+				 <ul class="nav">
+				   <li><a href="index.html">Home</a></li>
+				   <li><a href="sobre.html">Sobre</a></li>
+				   <li><a href="trainers.html">Treinos</a></li>
+				   <li><a href="classes.html">Professores</a></li>
+				   <li class="active"><a href="loja.php">Loja</a></li>
+				   <li><a href="pricing.html">Preços</a></li>
+				   <li><a href="contact.html">Contato</a></li>
+					 <li><a href="login.html">Login</a></li>
+				 </ul>
+				  <script type="text/javascript" src="js/nav.js"></script>
+			  </div><!-- end h_menu4 -->
+			 <div class="clear"></div>
+	  </div>
+	</div>
+	<!-- end menu -->
+
+		<div class="container-fluid">
+			<div class="col-md-2">
+				<div class="header-bottom header-bottom-menu">
+					<p>Menu</p>
+				</div>
+			</div>
+			<div class="col-md-10">
+				<div class="header-bottom header-bottom-menu">
+					<p>Loja</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="container-fluid">
+			<div class="col-md-2">
+				<div class="header-bottom header-bottom-opcao">
+					<ul class="nav nav1 nav-pills nav-stacked">
+						<li><a href="#">Whey</a></li>
+					</ul>
+					<ul class="nav nav1 nav-pills nav-stacked">
+						<li><a href="#">Termogênico</a></li>
+					</ul>
+					<ul class="nav nav1 nav-pills nav-stacked">
+						<li><a href="#">BCAA</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-10">
+			 	<iframe class="col-md-12" src="index.html" height="500px"></iframe>
+			</div>
+		</div>
+
+
+		 <div class="footer-bottom">
+		   <div class="container">
+		 	 <div class="row section group">
+				<div class="col-md-4">
+				   <h4 class="m_7">Inscreva-se para receber Newsletter</h4>
+				   <p class="m_8">Você ficará por dentro de todas as nossas promoções e eventos.</p>
+				      <form class="subscribe">
+			             <input type="text" value="Insert Email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Insert Email';}">
+					  </form>
+			          <div class="subscribe1">
+			            <a href="#">Enviar Email<i class="but_arrow"> </i></a>
+			          </div>
+				</div>
+				<div class="col-md-4">
+					<div class="f-logo">
+						<img src="images/logo.png" alt=""/>
+					</div>
+					<p class="m_9">A vontade de se preparar tem que ser maior do que a vontade de vencer. Vencer será consequência da boa preparação, Entre em contato conosco e agende uma aula gratuita.</p>
+					<p class="address">Telefone : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="m_10">(00) 222 666 444</span></p>
+					<p class="address">Email : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="m_10">projeto@meuprojeto.com</span></p>
+				</div>
+				<div class="col-md-4">
+					<ul class="list">
+						<h4 class="m_7">Menu</h4>
+						<li><a href="#">Sobre</a></li>
+						<li><a href="trainers.html">Treinos</a></li>
+						<li><a href="classes.html">Professores</a></li>
+						<li><a href="pricing.html">Preços</a></li>
+						<li><a href="loja.html">Loja</a></li>
+						<li><a href="login.html">Login</a></li>
+						<li><a href="contact.html">Contato</a></li>
+					</ul>
+				</div>
+				<div class="clear"></div>
+	  		  </div>
+		 	</div>
+		 </div>
+		 <div class="copyright">
+		  <div class="container">
+		    <div class="copy">
+		        <p>© 2018 Template by Marcos e Jean</p>
+		    </div>
+		    <div class="social">
+			   <ul>
+				  <li class="facebook"><a href="#"><span> </span></a></li>
+				  <li class="twitter"><a href="#"><span> </span></a></li>
+				  <li class="pinterest"><a href="#"><span> </span></a></li>
+				  <li class="google"><a href="#"><span> </span></a></li>
+				  <li class="tumblr"><a href="#"><span> </span></a></li>
+				  <li class="instagram"><a href="#"><span> </span></a></li>
+				  <li class="rss"><a href="#"><span> </span></a></li>
+			   </ul>
+		    </div>
+		   <div class="clear"></div>
+		  </div>
+	     </div>
+</body>
+</html>
---
+++ produtos.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta charset="utf-8">
+  	<title>Academia Mosntrão</title>
+  	<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+  	<link href="css/style.css" rel='stylesheet' type='text/css' />
+  	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+  	<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700' rel='stylesheet' type='text/css'>
+  	<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+  	<script src="js/jquery.min.js"></script>
+  	<!-- grid-slider -->
+  	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+  	<script type="text/javascript" src="js/jquery.contentcarousel.js"></script>
+    <title></title>
+  </head>
+  <body>
+    <div class="container" id="tourpackages-carousel">
+
+      <div class="row">
+
+        <div class="col-xs-18 col-sm-6 col-md-3">
+          <div class="thumbnail">
+            <img src="images/wheygold.jpg" alt="">
+              <div class="caption">
+                <h4>Thumbnail label</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, soluta, eligendi doloribus sunt minus amet sit debitis repellat. Consectetur, culpa itaque odio similique suscipit</p>
+                <p><a href="#" class="btn btn-info btn-xs" role="button">Button</a> <a href="#" class="btn btn-default btn-xs" role="button">Button</a></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xs-18 col-sm-6 col-md-3">
+          <div class="thumbnail">
+            <img src="http://placehold.it/500x300" alt="">
+              <div class="caption">
+                <h4>Thumbnail label</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, soluta, eligendi doloribus sunt minus amet sit debitis repellat. Consectetur, culpa itaque odio similique suscipit</p>
+                <p><a href="#" class="btn btn-info btn-xs" role="button">Button</a> <a href="#" class="btn btn-default btn-xs" role="button">Button</a></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xs-18 col-sm-6 col-md-3">
+          <div class="thumbnail">
+            <img src="http://placehold.it/500x300" alt="">
+              <div class="caption">
+                <h4>Thumbnail label</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, soluta, eligendi doloribus sunt minus amet sit debitis repellat. Consectetur, culpa itaque odio similique suscipit</p>
+                <p><a href="#" class="btn btn-info btn-xs" role="button">Button</a> <a href="#" class="btn btn-default btn-xs" role="button">Button</a></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xs-18 col-sm-6 col-md-3">
+          <div class="thumbnail">
+            <img src="http://placehold.it/500x300" alt="">
+              <div class="caption">
+                <h4>Thumbnail label</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, soluta, eligendi doloribus sunt minus amet sit debitis repellat. Consectetur, culpa itaque odio similique suscipit</p>
+                <p><a href="#" class="btn btn-info btn-xs" role="button">Button</a> <a href="#" class="btn btn-default btn-xs" role="button">Button</a></p>
+            </div>
+          </div>
+        </div>
+
+      </div><!-- End row -->
+
+    </div><!-- End container -->
+
+
+  </body>
+</html>
