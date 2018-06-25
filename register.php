bba120c901ba83a351da59a19b3cc68aaa1edb63
Marcos Roberto Albrecht <MarcosAlbrecht@users.noreply.github.com>
2018-06-25 20:21:41 -0300

    asdas

--- register.php
+++ register.php
@@ -47,7 +47,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 		 <div class="h_menu4"><!-- start h_menu4 -->
 		   <a class="toggleMenu" href="#">Menu</a>
 			 <ul class="nav">
-			   <li class="active"><a href="index.html">Home</a></li>
+			   <li class="active"><a href="index.php">Home</a></li>
 			   <li><a href="sobre.php">Sobre</a></li>
 			   <li><a href="trainers.php">Treinos</a></li>
 			   <li><a href="classes.php">Professores</a></li>
