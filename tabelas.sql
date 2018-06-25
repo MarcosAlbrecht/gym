408d61130034fbfbaa1713e0d3762b4e203f2333
Marcos Roberto Albrecht <MarcosAlbrecht@users.noreply.github.com>
2018-06-25 15:18:18 -0300

    marcos
    aaaaaaaaaaaa

--- tabelas.sql
+++ tabelas.sql
@@ -6,73 +6,80 @@ select * from products_has_venda;
 select * from bairro;
 
 
-CREATE SCHEMA IF NOT EXISTS `dbloja` DEFAULT CHARACTER SET utf8 ;
-USE `dbloja` ;
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
 
 -- -----------------------------------------------------
 -- Table `mydb`.`products`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dbloja`.`products` (
+CREATE TABLE IF NOT EXISTS `mydb`.`products` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
   `price` DECIMAL(10,2) NOT NULL,
   `image` VARCHAR(255) NOT NULL,
   `description` MEDIUMTEXT NOT NULL,
   `qtd` INT NOT NULL,
-  PRIMARY KEY (`id`));
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`estado`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dbloja`.`estado` (
+CREATE TABLE IF NOT EXISTS `mydb`.`estado` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(255) NULL,
-  PRIMARY KEY (`id`));
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`cidade`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dbloja`.`cidade` (
+CREATE TABLE IF NOT EXISTS `mydb`.`cidade` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`));
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`bairro`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dbloja`.`bairro` (
+CREATE TABLE IF NOT EXISTS `mydb`.`bairro` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`));
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`rua`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dbloja`.`rua` (
+CREATE TABLE IF NOT EXISTS `mydb`.`rua` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`));
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`tipousuario`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dbloja`.`tipousuario` (
+CREATE TABLE IF NOT EXISTS `mydb`.`tipousuario` (
   `id` INT NOT NULL,
   `tipo` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`));
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`usuario`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dbloja`.`usuario` (
+CREATE TABLE IF NOT EXISTS `mydb`.`usuario` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(255) NOT NULL,
+  `sobrenome` VARCHAR(255) NOT NULL,
   `cpf` VARCHAR(45) NOT NULL,
   `contato` VARCHAR(45) NOT NULL,
   `email` VARCHAR(255) NOT NULL,
@@ -85,25 +92,26 @@ CREATE TABLE IF NOT EXISTS `dbloja`.`usuario` (
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_usuario_estado1`
     FOREIGN KEY (`estado_id`)
-    REFERENCES `dbloja`.`estado` (`id`),
+    REFERENCES `mydb`.`estado` (`id`),
   CONSTRAINT `fk_usuario_cidade1`
     FOREIGN KEY (`cidade_id`)
-    REFERENCES `dbloja`.`cidade` (`id`),
+    REFERENCES `mydb`.`cidade` (`id`),
   CONSTRAINT `fk_usuario_bairro1`
     FOREIGN KEY (`bairro_id`)
-    REFERENCES `dbloja`.`bairro` (`id`),
+    REFERENCES `mydb`.`bairro` (`id`),
   CONSTRAINT `fk_usuario_rua1`
     FOREIGN KEY (`rua_id`)
-    REFERENCES `dbloja`.`rua` (`id`),
+    REFERENCES `mydb`.`rua` (`id`),
   CONSTRAINT `fk_usuario_tipousuario1`
     FOREIGN KEY (`tipousuario_id`)
-    REFERENCES `dbloja`.`tipousuario` (`id`));
+    REFERENCES `mydb`.`tipousuario` (`id`))
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`venda`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dbloja`.`venda` (
+CREATE TABLE IF NOT EXISTS `mydb`.`venda` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `valor` DECIMAL(10,2) NOT NULL,
   `date` VARCHAR(45) NOT NULL,
@@ -111,23 +119,61 @@ CREATE TABLE IF NOT EXISTS `dbloja`.`venda` (
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_venda_usuario1`
     FOREIGN KEY (`usuario_id`)
-    REFERENCES `dbloja`.`usuario` (`id`));
+    REFERENCES `mydb`.`usuario` (`id`))
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`products_has_venda`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dbloja`.`products_has_venda` (
+CREATE TABLE IF NOT EXISTS `mydb`.`products_has_venda` (
   `products_id` INT NOT NULL,
   `venda_id` INT NOT NULL,
   `quantidade` INT NULL,
   PRIMARY KEY (`products_id`, `venda_id`),
   CONSTRAINT `fk_products_has_venda_products`
     FOREIGN KEY (`products_id`)
-    REFERENCES `dbloja`.`products` (`id`),
+    REFERENCES `mydb`.`products` (`id`),
   CONSTRAINT `fk_products_has_venda_venda1`
     FOREIGN KEY (`venda_id`)
-    REFERENCES `dbloja`.`venda` (`id`));
+    REFERENCES `mydb`.`venda` (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`pagamento`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`pagamento` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `numeroCartao` VARCHAR(45) NOT NULL,
+  `nomeCartao` VARCHAR(45) NOT NULL,
+  `dataExpiracao` VARCHAR(45) NOT NULL,
+  `codigoSeguranca` VARCHAR(45) NOT NULL,
+  `dataPagamento` VARCHAR(45) NOT NULL,
+  `plano` VARCHAR(45) NOT NULL,
+  `usuario_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_pagamento_usuario1`
+    FOREIGN KEY (`usuario_id`)
+    REFERENCES `mydb`.`usuario` (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`treino`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`treino` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `treinoA` VARCHAR(255) NOT NULL,
+  `treinoB` VARCHAR(255) NOT NULL,
+  `treinoC` VARCHAR(255) NOT NULL,
+  `usuario_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_treino_usuario1`
+    FOREIGN KEY (`usuario_id`)
+    REFERENCES `mydb`.`usuario` (`id`))
+ENGINE = InnoDB;
+
 
 insert into products(name, price, image, description, qtd) values ("Whey gold 1KG", 130.90, "images/wheygold.jpg", "A Whey Gold 100% Protein ON é uma proteína de alto valor biológico e é muito usada por atletas de rendimento, praticantes de atividade física e até idosos que pretendem minimizar a perda muscular que ocorre com o processo de envelhecimento.", 10);
   insert into products(name, price, image, description, qtd) values ("Whey Max Titanium 1KG", 110.00, "images/whey-max-titanium.jpg", "O 100% Whey Protein Max Titanium 900 g é um suplemento proteico à base de proteína concentrada do soro do leite. Possui alto valor biológico, sendo rico em BCAA.", 10);
@@ -156,5 +202,5 @@ insert into products(name, price, image, description, qtd) values ("Whey gold 1K
   insert into rua(nome) values ("Rua Belo Horizonte  ");
   insert into rua(nome) values ("Rua Dom Pedro II");
 
-  insert into usuario(nome, cpf, contato, email, senha, estado_id, cidade_id, bairro_id, rua_id, tipousuario_id) values ("Marcos", 123456, 3333333, "admin", "admin", 1, 1, 5, 1, 1);
-  insert into usuario(nome, cpf, contato, email, senha, estado_id, cidade_id, bairro_id, rua_id, tipousuario_id) values ("Fausto Silva", 123456, 3333333, "fausto", "fausto", 2, 3, 2, 4, 3);
+  insert into usuario(nome, sobrenome, cpf, contato, email, senha, estado_id, cidade_id, bairro_id, rua_id, tipousuario_id) values ("Marcos", "Roberto", 123456, 3333333, "admin", "admin", 1, 1, 5, 1, 1);
+  insert into usuario(nome, sobrenome, cpf, contato, email, senha, estado_id, cidade_id, bairro_id, rua_id, tipousuario_id) values ("Fausto Silva", "Silva", 123456, 3333333, "fausto", "fausto", 2, 3, 2, 4, 3);
